4790b7b8da423311dcb7d181616865eebdc1b897
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-07-14 20:37:55 +0200

    initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3200/");
+      setData(response.data);
+      setIsLoading(false);
+
+    } catch (error) {
+      alert("An error occurred");
+    }
+  }
+
+  useEffect(()=>{
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    isLoading ?
+    (<span>En cours de chargement ...</span>)
+    : (
+      <div>
+      <h1>{data.restaurant.name}</h1>
+      <p>{data.restaurant.description}</p>
+       </div>
+
+    )
+   
   );
 }
 
