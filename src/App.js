b9cbe9389a8931944e6114e23eff4303c97c99b4
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-07-15 21:23:31 +0200

    push json informations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
+  const categories = data.categories;
+
+
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:3200/");
+      const response = await axios.get("https://my-deliveroo-app.herokuapp.com/");
       setData(response.data);
       setIsLoading(false);
 
@@ -26,9 +30,21 @@ function App() {
     (<span>En cours de chargement ...</span>)
     : (
       <div>
-      <h1>{data.restaurant.name}</h1>
-      <p>{data.restaurant.description}</p>
-       </div>
+        <div>
+          <h1>{data.restaurant.name}</h1>
+          <p>{data.restaurant.description}</p>
+          <img src={data.restaurant.picture} alt="restaurant" style={{width:400}}/>
+        </div>
+     
+       
+        {categories.map(category=>{
+          return( 
+            <Menu bigTitle = {category.name} meals={category.meals} />
+        
+          )})
+        }
+      
+      </div>
 
     )
    
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,31 @@
+import "./Menu.css";
+
+const Menu = (props) => {
+    const{ bigTitle, meals} = props;
+  return(
+      <>
+          <h1>{bigTitle}</h1>
+
+        <div className="menus-container">
+            {meals.map((meal)=>{
+                return(
+                    <div className="menu-container" key={meal.id}>
+                    <div className="col1">
+                        <h2>{meal.title}</h2>
+                        <p>{meal.description}</p>
+                        <h3>{meal.price}</h3>
+                    </div>
+                    <div className="col2">
+                        <img src={meal.picture} alt="menu" /> 
+                    </div>
+                  </div>
+                )
+
+            })}
+        </div>
+          
+      </>
+  );
+}
+
+export default Menu;
